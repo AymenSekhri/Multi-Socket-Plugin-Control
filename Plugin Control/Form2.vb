abbe2e3b28f48c7c3597e0b1243b6094bd1def7e
Aymen Sekhri <Aymen.sekhri@live.fr>
2019-08-09 01:28:46 +0200

    Update Form2.vb
    clean the code

--- Plugin Control/Form2.vb
+++ Plugin Control/Form2.vb
@@ -8,24 +8,5 @@
         PictureBox1.Dock = DockStyle.Fill
     End Sub
     Public oo As Boolean
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        'Dim hhh As String = My.Computer.FileSystem.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Plugins\type.txt")
-        'If hhh = "image" Then
-        '    Label1.Text = "Recived Image"
-        '    PictureBox1.Visible = True
-        '    TextBox1.Visible = False
-        '    Dim aymen = My.Computer.FileSystem.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\PlugControl_Images")
-        '    For Each x As String In aymen
-        '        PictureBox1.Image = Image.FromFile(aymen(aymen.Count - 1))
-        '    Next
-        'Else
-        '    Dim tetx As String = My.Computer.FileSystem.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Plugins\text.txt")
-        '    PictureBox1.Visible = False
-        '    TextBox1.Visible = True
-        '    TextBox1.Text = tetx
-        'End If
-
-
-        
-    End Sub
+    
 End Class
